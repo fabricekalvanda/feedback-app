08432acbe072107ef0d6ea3f48611021e185a0ae
fabricekalvanda <fabricekalvanda@gmail.com>
2022-10-04 11:59:18 -0700

    fixes State and useState Hook

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -5,13 +5,12 @@ function FeedbackItem() {
     const [rating, setRating] = useState(7)
     const [text, settext] = useState('This is an example of feedback item.')
 
-    
+
 
   return (
     <div className='card'>
         <div className="num-display">{rating}</div>
         <div className="text-display">{text}</div>
-        <button onClick={handleClick}>Click</button>
     
     </div>
   )
