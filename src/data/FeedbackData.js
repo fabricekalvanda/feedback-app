b7e534855da4cf8ce40cf6ef01c9efd55b3a94ea
fabricekalvanda <fabricekalvanda@gmail.com>
2022-10-06 14:56:27 -0700

    Managing Global State

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,23 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: 'Lorem ipsum odor amet, consectetuer adipiscing elit.'
+    },
+    {
+        id: 2,
+        rating: 9,
+        text: 'Lorem ipsum odor amet, consectetuer adipiscing elit. '
+    },
+    {
+        id: 3,
+        rating: 8,
+        text: 'Lorem ipsum odor amet, consectetuer adipiscing elit. '
+    },{
+        id: 4,
+        rating: 7,
+        text: 'Lorem ipsum odor amet, consectetuer adipiscing elit. '
+    },
+]
+
+export default FeedbackData
