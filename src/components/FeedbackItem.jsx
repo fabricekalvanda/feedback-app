2a6eb426c6be861826eca2b1905e183693647842
fabricekalvanda <fabricekalvanda@gmail.com>
2022-10-11 10:36:36 -0700

    Adds Events and Prop Drilling

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,14 +1,18 @@
+import { FaTimes } from 'react-icons/fa'
 import PropTypes from 'prop-types'
 import Card from './shared/Card'
 import React from 'react'
 import {useState} from 'react'
 
 
-function FeedbackItem({item}) {
-
+function FeedbackItem({item, handleDelete}) {
   return (
     <Card>
         <div className="num-display">{item.rating}</div>
+        <button onClick={() => handleDelete(item.id)}
+        className='close'>
+          <FaTimes color='purple' />
+        </button>
         <div className="text-display">{item.text}</div>
 
     </Card>
