848f05d7bfd2b3327f5dcfced897d49bcdc08e9a
fabricekalvanda <fabricekalvanda@gmail.com>
2022-10-12 09:52:11 -0700

    Adds Feedback Stats Component and Reactivity

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
+import FeedbackStats from "./components/FeedbackStats"
 import FeedbackData from "./data/FeedbackData"
 
 function App() {
@@ -8,13 +9,14 @@ function App() {
     const deleteFeedback = (id) => {
         if (window.confirm('Are you sure you want to delete?')) {
             setFeedback(feedback.filter((item) => item.id !== id))
-            
+
         }
     }
     return (
         <>
             <Header />
             <div className='container'>
+                <FeedbackStats feedback={feedback} />
                 <FeedbackList feedback={feedback} 
                 handleDelete = {deleteFeedback}/>
                 
