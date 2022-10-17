48065f6d5b93216e9cf480d3a0cec451f0e09d3d
fabricekalvanda <fabricekalvanda@gmail.com>
2022-10-17 09:45:22 -0700

    Adds Real-Time Validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -6,20 +6,41 @@ import Button from './shared/Button'
 
 function FeedbackForm() {
     const [text, setText] = useState('')
+    const [btnDisabled, setBtnDisable] = useState(true)
+    const [message, setMessage] = useState('')
 
     const handleTextChange = (e) => {
+        if(text === ''){
+            setBtnDisable(true)
+            setMessage(null)
+        } else if(text !== '' && text.trim().length <= 10) {
+            setMessage('Text must be at least 10 characters')
+            setBtnDisable(true)
+        }else {
+            setMessage(null)
+            setBtnDisable(false)
+        }
+
+        
         setText(e.target.value)
     }
     return (
+    
       <Card>
-        <h2>How would you rate your service with us?</h2>
-        {/* @todo - rating select component */}
-        <div className="input-group">
-            <input onChange={handleTextChange} type='text' 
-                placeholder='Write your review'
-                value={text} />
-            <Button type="submit">Send</Button>
-        </div>
+        <form>
+            <h2>How would you rate your service with us?</h2>
+            {/* @todo - rating select component */}
+            <div className="input-group">
+                <input onChange={handleTextChange} type='text' 
+                    placeholder='Write your review'
+                    value={text} 
+                />
+                <Button type="submit" isDisabled={btnDisabled}>
+                    Send
+                </Button>
+            </div>
+            {message && <div className='message'>{message}</div>}
+        </form>
       </Card>
     )
   }
