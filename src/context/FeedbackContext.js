183602f79dd82eca980525971bc2931fa83f5346
fabricekalvanda <fabricekalvanda@gmail.com>
2022-10-28 12:55:00 -0700

    Adds Edit Feedback Event

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -23,17 +23,31 @@ export const FeedbackProvider = ({children}) => {
         }
     ])
 
+    const [feedbackEdit, setFeedbackEdit] = useState({
+        item: {},
+        edit: false
+    })
+
+    //Add feedback
     const addFeedback = (newFeedback) => {
         newFeedback.id = uuidv4()
         setFeedback([newFeedback, ...feedback])
     }
 
+    // Delete feedback
     const deleteFeedback = (id) => {
         if (window.confirm('Are you sure you want to delete?')) {
             setFeedback(feedback.filter((item) => item.id !== id))
         }
     }
 
+    // Set item to be updated
+    const editFeedback = (item) => {
+        setFeedbackEdit({
+            item,
+            edit: true
+        })
+    }
 
     return( 
         <FeedbackContext.Provider
@@ -41,6 +55,7 @@ export const FeedbackProvider = ({children}) => {
                 feedback,
                 deleteFeedback,
                 addFeedback,
+                editFeedback,
             }}
         >
             {children}
